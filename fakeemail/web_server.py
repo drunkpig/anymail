e261af4fd9889b0fd94442786e621e6dc4a9e8fb
Jon <xuchaoo@gmail.com>
2020-01-12 15:20:30 +0800

    upd

--- fakeemail/web_server.py
+++ fakeemail/web_server.py
@@ -13,6 +13,61 @@ except ImportError:
     import simplejson as json
 
 
+class EmailHtmlDisplay(Resource):
+
+    def __init__(self, name, storage):
+        Resource.__init__(self)
+        self.name = name
+        self.storage = storage
+
+    def render_GET(self, request):
+        email_list = self.storage.get_for_name(self.name)
+        decoded_email_list = []
+        available_content_types = set()
+        if email_list:
+            for msg, attachments in email_list:
+                parts = {}
+                parts['headers'] = msg.items()
+                parts['content_type'] = msg.get_content_type()
+                for part in msg.walk():
+                    # multipart/* are just containers, and things that
+                    # don't start with 'text/' are assumed to be
+                    # attachments
+                    is_multipart = (part.get_content_maintype() == 'multipart')
+                    is_text = part.get_content_type().startswith('text/')
+                    if is_multipart or not is_text:
+                        continue
+
+                    content_type = part.get_content_type()
+                    # Assume that any non-text types are just attachments
+                    if content_type.startswith('text/'):
+                        payload = part.get_payload(decode=True)
+                        if isinstance(payload, bytes):
+                            output = payload.decode('utf-8')
+                        else:
+                            output = payload
+
+                    parts[part.get_content_subtype()] = output
+                    available_content_types.add(part.get_content_subtype())
+
+                # Output the names of the attachments.
+                for i, attachment_path in enumerate(attachments, start=1):
+                    heading = 'attachment {}'.format(i)
+                    parts[heading] = os.path.basename(attachment_path)
+                    available_content_types.add(heading)
+
+                decoded_email_list.append(parts)
+
+        data = {'email_list': decoded_email_list,
+                'address_name': self.name,
+                'content_types': list(available_content_types)}
+
+        # env = Environment(loader=PackageLoader('fakeemail', 'templates'))
+        # template = env.get_template('message_display.html')
+
+        return decoded_email_list[0]['html']
+
+
 class WebMessageDisplay(Resource):
 
     def __init__(self, name, storage):
@@ -69,6 +124,9 @@ class WebMessageDisplay(Resource):
 
 
 class WebMessageRootDisplay(Resource):
+    """
+    这是网页入口，首月，显示Email列表等
+    """
 
     def __init__(self, name, storage):
         Resource.__init__(self)
@@ -162,9 +220,10 @@ class WebMessageRouter(Resource):
             elif name == 'file':
                 return File(self.temp_dir)
 
-            return WebMessageDisplay(name, self.storage)
+            #return WebMessageDisplay(name, self.storage)  # email地址
+            return EmailHtmlDisplay(name, self.storage)
         else:
-            return WebMessageRootDisplay(name, self.storage)
+            return WebMessageRootDisplay(name, self.storage) # 首页
 
 
 def makeWebService(web_interface, web_port, storage):
